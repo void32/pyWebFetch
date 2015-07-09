abc2ec2327871e2b67d20d97bea30951f912a499
void32 <mortenalbecknielsen@gmail.com>
2015-07-09 06:42:50 +0200

    Indented comment.

--- pyWebFetch.py
+++ pyWebFetch.py
@@ -59,7 +59,7 @@ def download_file(url):
     r = session.get(url, stream=True)
     with open(local_filename, 'wb') as f:
         for chunk in r.iter_content(chunk_size=1024):
-            if chunk: # filter out keep-alive new chunks
+            if chunk:  # filter out keep-alive new chunks
                 f.write(chunk)
                 f.flush()
     return local_filename
